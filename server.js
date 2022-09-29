e7a77c6cd2cdf0cc83af45888964d68ab51d9172
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-29 08:40:21 +0530

    Port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 // require("./redis");
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
