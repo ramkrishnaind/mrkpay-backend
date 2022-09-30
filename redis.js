0daf3b1acac6d995ccc04737bfba3880d210c4db
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-09-30 12:56:28 +0530

    Update redis.js
    all reverted

--- redis.js
+++ redis.js
@@ -7,7 +7,7 @@ const Redis = require("ioredis");
 //   "rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309"
 // );
 //rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309
-let client = new Redis("rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309");
+let client = new Redis("redis://13.115.187.17:6379");
 client
   .connect()
   .then(() => {})
