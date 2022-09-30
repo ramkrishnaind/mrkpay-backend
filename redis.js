69a75eeb40e57df53d14353c8f33f15f721d3aa4
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-09-30 12:51:15 +0530

    Update redis.js
    test 7

--- redis.js
+++ redis.js
@@ -7,7 +7,7 @@ const Redis = require("ioredis");
 //   "rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309"
 // );
 //rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309
-let client = new Redis(process.env.REDIS_URL);
+let client = new Redis("rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309");
 client
   .connect()
   .then(() => {})
