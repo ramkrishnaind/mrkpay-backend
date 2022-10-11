0e18d64e4bd880bbea136550262e863ac45915ea
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-10-11 11:50:43 +0530

    new redis server connection

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-console.log("process.env.REDIS_URL", process.env.REDIS_URL);
+// console.log("process.env.REDIS_URL", process.env.REDIS_URL);
