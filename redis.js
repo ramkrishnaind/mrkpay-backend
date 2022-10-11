b6c2e5cee39b7af11ae1c60fda3ab69609e4ed85
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-10-11 17:39:42 +0530

    Update redis.js
    Light Sail Redis.js codes

--- redis.js
+++ redis.js
@@ -1,18 +1,21 @@
 const redis = require("redis");
 // const client = redis.createClient();
 const Redis = require("ioredis");
-// const client = redis.createClient(6379, "18.182.50.38");
+// const client = redis.createClient(6379, "3.85.164.141");
 // client.connect();
-let client = new Redis("redis://43.206.109.210:6379");
-// let client = new Redis("rediss://:18.182.50.38:38309");
+// let client = new Redis(
+//   "rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:383
+09"
+// );
 //rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309
-// let client = new Redis("redis://13.115.187.17:6379");
-// client
-//   .connect()
-//   .then(() => {})
-//   .catch((error) => {
-//     console.log(error);
-//   });
+//let client = new Redis("redis://18.182.50.38:6379");
+let client = new Redis("redis://43.206.109.210:6379");
+//client
+//  .connect()
+//  .then(() => {})
+//  .catch((error) => {
+//    console.log(error);
+//  });
 client.on("connect", function () {
   console.log("Connected!");
 });
