482b2083166548e0a7e1230b406c95cf281c309e
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-11-07 13:07:02 +0530

    Coin generation logic modified

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,6 +5,10 @@ const {
   addDoc,
   getDocs,
   collection,
+  query,
+  where,
+  orderBy,
+  limit,
 } = require("firebase/firestore");
 const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 const { use } = require("../routers/usersRouter");
@@ -44,28 +48,45 @@ async function addGeneratedCoin(req, res) {
   let targetObj;
   try {
     const docRef = doc(db, "users", uid);
+    const coinsRef = collection(db, "coinTransaction");
+    // const coinsRef = doc(db, "coinTransaction", uid);
     const obj = await getDoc(docRef);
-    targetObj = obj.data();
-    console.log("targetObj", obj);
-    if (targetObj) {
-      targetObj.coinsGenerated += 1;
-      await setDoc(docRef, targetObj);
-      const coinTransaction = {
-        createdBy: uid,
-        coinGenerated: 1,
-        createdAt: new Date().toISOString(),
-      };
+    // const objCoins = await getDoc(coinsRef);
+    const q = query(
+      coinsRef,
+      where("createdBy", "==", uid),
+      orderBy("createdAt", "desc"),
+      limit(1)
+    );
+
+    targetObj = {
+      createdBy: uid,
+      createdAt: new Date().toISOString(),
+    };
+    const coinSnapshot = await getDocs(q);
+    if (
+      coinSnapshot.size === 0 ||
+      coinSnapshot.docs[0].data().createdAt.slice(0, 10) !==
+        new Date().toISOString().slice(0, 10)
+    ) {
+      targetObj.coinsGenerated = 1;
       const document = await addDoc(
         collection(db, "coinTransaction"),
-        coinTransaction
+        targetObj
       );
+      return res.json({ status: "success" });
+    } else {
+      const key = coinSnapshot.docs[0].id;
+      // doc.data() is never undefined for query doc snapshots
+      const coinData = coinSnapshot.docs[0].data();
+      targetObj.coinGenerated = (coinData?.coinGenerated || 0) + 1;
+      const document = await setDoc(doc(db, "coinTransaction", key), targetObj);
       return res.json({ status: "success" });
     }
   } catch (e) {
     console.log(e);
     return res.json({ status: "error" });
   }
-  return res.json({ status: "error" });
 }
 async function getAllUsers(req, res) {
   const snapshot = await getDocs(collection(db, "users"));
