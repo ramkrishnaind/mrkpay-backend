da386005b8b2e930520c03b9f794e92b46d1d929
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-11-21 18:44:55 +0530

    new ec2 conected

--- redis.js
+++ redis.js
@@ -8,7 +8,7 @@ const Redis = require("ioredis");
 // );
 //rediss://:1b20295202064b1697c39dbfa009cdc7@us1-adapting-oarfish-38309.upstash.io:38309
 //let client = new Redis("redis://18.182.50.38:6379");
-let client = new Redis("redis://18.237.149.90:6379");
+let client = new Redis("redis://13.126.53.250:6379");
 //client
 //  .connect()
 //  .then(() => {})
